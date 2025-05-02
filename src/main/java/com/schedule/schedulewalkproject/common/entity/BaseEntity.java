8df06c0fddbb10b7ba062d830024c74988f33fe7
yunhyeok_lee <yuy9611@gmail.com>
2025-05-02 17:08:42 +0900

    feat : BaseEntity 생성

--- src/main/java/com/schedule/schedulewalkproject/common/entity/BaseEntity.java
+++ src/main/java/com/schedule/schedulewalkproject/common/entity/BaseEntity.java
@@ -11,16 +11,19 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
+/**
+ * abstract class 로 하는 이유는 상속 받아서 사용하도록 제한하기 위해
+ */
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+@MappedSuperclass // JPA에서 공통 매핑 정보를 상속받을 때 사용하는 어노테이션
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 생성시점이나 수정 시점에 자동으로 특정 작업을 해주기위해 사용 (시간 작업)
+public abstract class BaseEntity {
 
 	@CreatedDate
-	@Column(updatable = false)
+	@Column(updatable = false) // 생성 시간 수정 불가능
 	private LocalDateTime createdAt;
 
-	@LastModifiedDate
+	@LastModifiedDate // 마지막으로 수정한 시간으로 업데이트
 	private LocalDateTime updatedAt;
 
 }
