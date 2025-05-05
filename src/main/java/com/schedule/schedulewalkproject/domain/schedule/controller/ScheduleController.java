180f276718549d59362b06a4164903aa269c20a2
yunhyeok_lee <yuy9611@gmail.com>
2025-05-06 03:03:49 +0900

    feat : 일정 전체조회 기능 추가

--- src/main/java/com/schedule/schedulewalkproject/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/schedule/schedulewalkproject/domain/schedule/controller/ScheduleController.java
@@ -2,14 +2,18 @@ package com.schedule.schedulewalkproject.domain.schedule.controller;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleRequestDto;
-import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleResponseDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleCreatRequestDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleCreatResponseDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleListResponseDto;
 import com.schedule.schedulewalkproject.domain.schedule.service.ScheduleService;
 
 @RestController   // @Controller + @ResponseBody
@@ -19,13 +23,46 @@ public class ScheduleController {
 
 	private final ScheduleService scheduleService;
 
+	// 1. 일정 생성
 	@PostMapping
-	public ResponseEntity<ScheduleResponseDto> save(@RequestBody ScheduleRequestDto scheduleRequestDto) {
+	public ResponseEntity<ScheduleCreatResponseDto> save(@RequestBody ScheduleCreatRequestDto scheduleRequestDto) {
 
-		ScheduleResponseDto responseDto = scheduleService.save(scheduleRequestDto);
+		ScheduleCreatResponseDto responseDto = scheduleService.save(scheduleRequestDto);
 
 		return ResponseEntity.ok().body(responseDto);
-
 	}
 
+	// 2. 일정 전체 조회
+	@GetMapping("/schedule")  // 전체 조회
+	public ResponseEntity<ScheduleListResponseDto> findAll(){
+
+		ScheduleListResponseDto scheduleResponseDto = scheduleService.findAll();
+
+		return ResponseEntity.ok().body(scheduleResponseDto);
+	}
+	// 3. 단건 일정 조회
+	@GetMapping("/schedule/{id}")
+	public ResponseEntity<ScheduleCreatResponseDto> findbyId(@PathVariable Long id){
+
+		ScheduleCreatResponseDto scheduleResponseDto = scheduleService.findbyId();
+
+		return ResponseEntity.ok().body(scheduleResponseDto);
+	}
+
+	// 4. 일정 수정
+	@PatchMapping("/schedule/{id}")
+	public ResponseEntity<ScheduleCreatResponseDto> update(@RequestBody ScheduleCreatRequestDto scheduleRequestDto) {
+
+		ScheduleCreatResponseDto scheduleResponseDto = scheduleService.update(scheduleRequestDto);
+
+		return ResponseEntity.ok().body(scheduleResponseDto);
+	}
+
+	// @DeleteMapping("/schedule/{id}")
+	// public ResponseEntity<ScheduleCreatResponseDto> delete(@PathVariable Long id) {
+	//
+	// 	scheduleService.delete(id);
+	//
+	// 	return ResponseEntity.ok().body();
+	// }
 }
--- src/main/java/com/schedule/schedulewalkproject/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/schedule/schedulewalkproject/domain/schedule/service/ScheduleService.java
@@ -1,9 +1,14 @@
 package com.schedule.schedulewalkproject.domain.schedule.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
-import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleRequestDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleCreatRequestDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleCreatResponseDto;
+import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleListResponseDto;
 import com.schedule.schedulewalkproject.domain.schedule.dto.ScheduleResponseDto;
 import com.schedule.schedulewalkproject.domain.schedule.entity.Schedule;
 import com.schedule.schedulewalkproject.domain.schedule.repository.ScheduleRepository;
@@ -14,13 +19,50 @@ public class ScheduleService {
 
 	private final ScheduleRepository scheduleRepository;
 
-	public ScheduleResponseDto save(ScheduleRequestDto scheduleRequestDto) {
+	// 1. 일정 생성 service
+	public ScheduleCreatResponseDto save(ScheduleCreatRequestDto scheduleRequestDto) {
 
 		Schedule schedule = new Schedule(scheduleRequestDto.getTitle(), scheduleRequestDto.getContent());
 
 		Schedule savedSchedule = scheduleRepository.save(schedule);
 
-		return new ScheduleResponseDto(savedSchedule.getId(), savedSchedule.getTitle(), savedSchedule.getContent(),savedSchedule.getCreatedAt(),savedSchedule.getUpdatedAt());
+		return new ScheduleCreatResponseDto(savedSchedule.getId(), savedSchedule.getTitle(), savedSchedule.getContent(),savedSchedule.getCreatedAt(),savedSchedule.getUpdatedAt());
 
 	}
+	/*
+	* 2. 일정 전체 조회
+	  * DB에서 모든 Schedule 데이터를 전체 조회해서 List에 저장
+	  * 조회한 Schedule 엔티티 리스트를  Dto List로 변환 (stream 사용)
+	 */
+
+	public ScheduleListResponseDto findAll() {
+		List<Schedule> schedules = scheduleRepository.findAll();
+
+		List<ScheduleResponseDto> scheduleDtos = schedules.stream()
+			.map(schedule -> new ScheduleResponseDto(
+				// 각 Schedule 객체를 -> ScheduleResponseDto 객체로 변환
+				schedule.getId(),
+				schedule.getTitle(),
+				schedule.getContent(),
+				schedule.getCreatedAt(),
+				schedule.getUpdatedAt()
+			))
+			.collect(Collectors.toList()); // 변환된 dto 객체들을 전부 리스트로 모아준다.
+		return new ScheduleListResponseDto(scheduleDtos);
+	}
+
+	public ScheduleCreatResponseDto findbyId() {
+
+		return null;
+	}
+
+	public ScheduleCreatResponseDto update(ScheduleCreatRequestDto scheduleRequestDto) {
+		return null;
+	}
+
+	public void delete(Long id) {
+
+	}
+
+
 }
