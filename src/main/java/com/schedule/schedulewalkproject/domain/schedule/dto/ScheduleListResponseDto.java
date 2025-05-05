f2e8cfcb49ff67b6e2b0e0961a9cb4ff838eeacd
yunhyeok_lee <yuy9611@gmail.com>
2025-05-06 03:03:07 +0900

    feat : 일정 전체 조회 dto 추가

---
+++ src/main/java/com/schedule/schedulewalkproject/domain/schedule/dto/ScheduleListResponseDto.java
@@ -0,0 +1,18 @@
+package com.schedule.schedulewalkproject.domain.schedule.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import com.schedule.schedulewalkproject.domain.schedule.entity.Schedule;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleListResponseDto {
+	// private List<ScheduleCreatResponseDto> scheduleCreatResponseDtoList;
+	private List<ScheduleResponseDto> scheduleResponseDtos;
+}
+
+
