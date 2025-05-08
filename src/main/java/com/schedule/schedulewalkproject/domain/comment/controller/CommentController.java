1cd7156bebdda76553e4c13f68e1214bd0a189ac
yunhyeok_lee <yuy9611@gmail.com>
2025-05-08 20:09:37 +0900

    feat : 댓글 상세 조회 추가

--- src/main/java/com/schedule/schedulewalkproject/domain/comment/controller/CommentController.java
+++ src/main/java/com/schedule/schedulewalkproject/domain/comment/controller/CommentController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentCreatRequestDto;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentCreatResponseDto;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentListResponseDto;
+import com.schedule.schedulewalkproject.domain.comment.dto.CommentResponseDto;
+import com.schedule.schedulewalkproject.domain.comment.dto.CommentUpdateRequestDto;
 import com.schedule.schedulewalkproject.domain.comment.service.CommentService;
 
 @RestController
@@ -19,28 +22,44 @@ public class CommentController {
 
 	private final CommentService commentService;
 
-	/*
-	 * 1. 댓글 작성
-	 */
+
+	// 1. 댓글 작성
 	@PostMapping("/schedules/{scheduleId}/comments")
 	public ResponseEntity<CommentCreatResponseDto> save(
 		@PathVariable Long scheduleId,
 		@RequestBody CommentCreatRequestDto commentCreatRequestDto) {
 
-		CommentCreatResponseDto responseDto = commentService.save(scheduleId,commentCreatRequestDto);
+		CommentCreatResponseDto responseDto = commentService.save(scheduleId, commentCreatRequestDto);
 
 		return ResponseEntity.ok().body(responseDto);
 	}
-	/*
-	 * 2. 댓글 조회
-	 */
+
+
+	// 2. 댓글 전체 조회
 	@GetMapping("/schedules/{scheduleId}/comments")
-	public ResponseEntity<CommentListResponseDto> findAll(){
+	public ResponseEntity<CommentListResponseDto> findAll() {
 
 		CommentListResponseDto commentListResponseDto = commentService.findAll();
 
 		return ResponseEntity.ok().body(commentListResponseDto);
 	}
 
+	@GetMapping
+	public ResponseEntity<CommentResponseDto> findbyId(@PathVariable Long id){
 
+		CommentResponseDto commentResponseDto = commentService.findbyId(id);
+
+		return ResponseEntity.ok().body(commentResponseDto);
+	}
+
+	// 3. 댓글 수정
+	@PatchMapping("/schedules/{scheduleId}/comments/{id}")
+	public ResponseEntity<CommentResponseDto> updateComment(
+		@PathVariable Long id,
+		@RequestBody CommentUpdateRequestDto commentUpdateRequestDto) {
+
+		CommentResponseDto commentResponseDto = commentService.updateComment(id, commentUpdateRequestDto);
+
+		return ResponseEntity.ok().body(commentResponseDto);
+	}
 }
--- src/main/java/com/schedule/schedulewalkproject/domain/comment/service/CommentService.java
+++ src/main/java/com/schedule/schedulewalkproject/domain/comment/service/CommentService.java
@@ -1,15 +1,19 @@
 package com.schedule.schedulewalkproject.domain.comment.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentCreatRequestDto;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentCreatResponseDto;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentListResponseDto;
 import com.schedule.schedulewalkproject.domain.comment.dto.CommentResponseDto;
+import com.schedule.schedulewalkproject.domain.comment.dto.CommentUpdateRequestDto;
 import com.schedule.schedulewalkproject.domain.comment.entity.Comment;
 import com.schedule.schedulewalkproject.domain.comment.repository.CommentRepository;
 
@@ -19,7 +23,10 @@ public class CommentService {
 
 	private final CommentRepository commentRepository;
 
-	// 1. 댓글 작성
+	/*
+	 * 1. 댓글 작성
+	 * scheduleId 값에 따라 댓글 작성
+	 */
 	public CommentCreatResponseDto save(Long scheduleId, CommentCreatRequestDto commentCreatRequestDto) {
 
 		Comment comment = new Comment(commentCreatRequestDto.getContent());
@@ -31,7 +38,10 @@ public class CommentService {
 
 	}
 
-	// 2. 댓글 조회
+	/*
+	 * 2. 댓글 조회
+	 * 스케줄 조회 시 댓글도 같이 조회
+	 */
 	public CommentListResponseDto findAll() {
 
 		List<Comment> commentList = commentRepository.findAll();
@@ -46,4 +56,34 @@ public class CommentService {
 			.collect(Collectors.toList());
 		return new CommentListResponseDto(commentResponseDtos);
 	}
+
+	/*
+	 * 3. 댓글 상세 조회
+	 */
+	public CommentResponseDto findbyId(Long id) {
+
+		Optional<Comment> optionalComment = commentRepository.findById(id);
+
+		if (optionalComment.isEmpty()){
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+
+		Comment findcomment = optionalComment.get();
+		return new CommentResponseDto(
+			findcomment.getId(),
+			findcomment.getContent(),
+			findcomment.getCreatedAt(),
+			findcomment.getUpdatedAt()
+		);
+	}
+	/*
+	 * 3. 댓글 수정
+	 */
+	public CommentResponseDto updateComment(Long id, CommentUpdateRequestDto commentUpdateRequestDto) {
+
+
+	return null;
+	}
+
+
 }
